de103a8d07c7076fa0b0477d2b38ed533d40010e
abulkhairsohag <sohag.ice.ru@gmail.com>
2019-12-03 18:01:06 +0600

    connection mande with db

--- config/config.php
+++ config/config.php
@@ -1,8 +1,8 @@
 <?php
 define("DB_HOST", "localhost");
-define("DB_USER", "sattit_dealer_new");
-define("DB_PASS", "&v%Q*iQR)~~!");
-define("DB_NAME", "sattit_dealer_new");
+define("DB_USER", "root");
+define("DB_PASS", "abulkhair45");
+define("DB_NAME", "dealer3");
 
 date_default_timezone_set('Asia/Dhaka');
 

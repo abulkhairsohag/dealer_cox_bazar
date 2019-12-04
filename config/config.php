00b6df37145aadf1894ae32394eef2e37b671397
abulkhairsohag <sohag.ice.ru@gmail.com>
2019-12-04 11:47:52 +0600

    database renamed

--- config/config.php
+++ config/config.php
@@ -2,7 +2,7 @@
 define("DB_HOST", "localhost");
 define("DB_USER", "root");
 define("DB_PASS", "abulkhair45");
-define("DB_NAME", "dealer3");
+define("DB_NAME", "dealer_cox_bazar");
 
 date_default_timezone_set('Asia/Dhaka');
 
